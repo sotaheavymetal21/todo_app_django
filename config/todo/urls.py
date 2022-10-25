58e86e700d1e12153bbd0f81b7c4f00cd7fe186a
Apple <apple@Sotanomakku.local>
2022-10-25 22:00:34 +0900

    first commit

---
+++ config/todo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import TodoDetail, TodoList, TodoCreate, TodoUpdate
+
+urlpatterns = [
+    path("", TodoList.as_view(), name="list"),
+    path("detail/<int:pk>", TodoDetail.as_view(), name="detail"),
+    path("create/", TodoCreate.as_view(), name="create"),
+    path("update/<int:pk>", TodoUpdate.as_view(), name="update")
+]
---
+++ config/todo/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+
+from django.urls import reverse_lazy
+
+from .models import Todo
+
+
+class TodoList(ListView):
+    model = Todo
+    context_object_name: "tasks"
+
+
+class TodoDetail(DetailView):
+    model = Todo
+    context_object_name = "task"
+
+
+class TodoCreate(CreateView):
+    model = Todo
+    fields = "__all__"
+    success_url = reverse_lazy("list")
+
+
+class TodoUpdate(UpdateView):
+    model = Todo
+    fields = "__all__"
+    success_url = reverse_lazy("list")
